f686ca64155a825ca3796a1a7b3c8154d95dcba1
Faraz Khan <faraz.khan14158124@gmail.com>
2025-10-28 20:06:23 +0530

    Vapi Ai Assistant added to the widget and tested our assistant on dashboard

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,19 +1,43 @@
 "use client"
-import { useQuery, useMutation } from "convex/react"
-import { api } from "@workspace/backend/_generated/api"
+
+import { useVapi } from "@/modules/widget/hooks/use-vapi"
 import { Button } from "@workspace/ui/components/button"
 
 export default function Page() {
-  const users = useQuery(api.users.getMany)
-  const addUser = useMutation(api.users.add)
+  const { isConnected,
+    isConnecting,
+    isSpeaking,
+    transcript,
+    startCall,
+    endCall } = useVapi()
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <p>App/Widget</p>
-        <Button onClick={() => addUser({ userName: "Name" })}> Add User</Button>
-        {users?.map((user, idx) => (
-          <p key={idx}>{idx} : {user.name}</p>
-        ))}
+        <h1>Chat Widget</h1>
+        <div className="border border-gray-400 w-[50vw] h-[500px] relative p-4 flex flex-col gap-2 bg-gray-800 overflow-y-auto rounded-xl">
+          {
+            transcript.map((trc, idx) => {
+              if (trc.role === 'user') {
+                return (
+                  <p key={idx} className="rounded-lg max-w-[60%] bg-gray-300 p-2 self-end">
+                    {trc.text}
+                  </p>
+                )
+              } else {
+                return (
+                  <p key={idx} className="text-white rounded-lg max-w-[60%] bg-gray-900 p-2 self-start">
+                    {trc.text}
+                  </p>
+                )
+              }
+            })
+          }
+        </div>
+        <div className="flex flex-row w-[50vw] items-center justify-between">
+          <Button className="w-[40%] cursor-pointer" onClick={() => startCall()}>Start</Button>
+          <Button className="w-[40%] cursor-pointer" variant={'destructive'} onClick={() => endCall()}>End</Button>
+        </div>
       </div>
     </div>
   )
